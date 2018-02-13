7f9271c5ed5cbeca2659354cd12a5a59e018487c
Simon Bausch <simon.bausch@cellent.de>
2018-02-13 17:44:59 +0100

    started History implementation

---
+++ src/main/java/org/unclesniper/arceye/stage/CacheTransition.java
@@ -0,0 +1,8 @@
+package org.unclesniper.arceye.stage;
+
+public enum CacheTransition {
+	THE_MORE_THINGS_CHANGE,
+	JESUS_SAVES,
+	LIFT_ME_UP,
+	MAP
+}
--- src/main/java/org/unclesniper/arceye/stage/History.java
+++ src/main/java/org/unclesniper/arceye/stage/History.java
@@ -15,14 +15,134 @@ package org.unclesniper.arceye.stage;
  *                -> s3
  */
 
-public class History {
+public class History<StateT> implements NodeIO<Snapshot<StateT>> {
 
-	public static class Snapshot {
+	public static class Snapshot<StateT> {
 
-		//TODO
+		public static class NextLink<StateT> {
+
+			private long nextID;
+
+			private Snapshot<StateT> next;
+
+		}
+
+		private final History<StateT> history;
+
+		private long id;
+
+		private final long stratum;
+
+		private final StateT state;
+
+		private final long previousID;
+
+		private Snapshot<StateT> previous;
+
+		private List<NextLink<Snapshot>> nextLinks;
+
+		public Snapshot(History<StateT> history, StateT state) {
+			this.history = history;
+			id = -1l;
+			stratum = 0l;
+			this.state = state;
+			previousID = -1l;
+		}
+
+		public History<StateT> getHistory() {
+			return history;
+		}
+
+		public long getID() {
+			return id;
+		}
+
+		public long getStratum() {
+			return stratum;
+		}
+
+		public StateT getState() {
+			return state;
+		}
+
+		public long getPreviousID() {
+			return previousID;
+		}
+
+		public Snapshot<StateT> getPrevious() {
+			return previous;
+		}
+
+		public Iterable<NextLink<Snapshot>> getNextLinks() {
+			return nextLinks;
+		}
 
 	}
 
-	//TODO
+	public static final int DEFAULT_MAX_CACHED_STRATA = 1;
+
+	private StageFile stage;
+
+	private NodeIO<StateT> stateIO;
+
+	private ByteBuffer ioBuffer;
+
+	private int maxCachedStrata = History.DEFAULT_MAX_CACHED_STRATA;
+
+	private Snapshot<StateT> currentState;
+
+	public History(StateT initialState, StageFile stage, NodeIO<StateT> stateIO) {
+		this.stage = stage;
+		this.stateIO = stateIO;
+		currentState = new Snapshot<StateT>(this, initialState);
+	}
+
+	public StageFile getStage() {
+		return stage;
+	}
+
+	public void setStage(StageFile stage) {
+		if(state == this.stage)
+			return;
+		if(stateIO != null) {
+			if(this.stage == null)
+				currentState.saveAll(stage);
+			else if(stage == null)
+				currentState.liftAll();
+			else
+				currentState = currentState.mapToStage(stage);
+		}
+		this.stage = stage;
+	}
+
+	public NodeIO<StateT> getStateIO() {
+		return stateIO;
+	}
+
+	public void setStateIO(NodeIO<StateT> stateIO) {
+		if(stateIO == this.stateIO)
+			return;
+		if(stage != null) {
+			//TODO
+		}
+		this.stateIO = stateIO;
+	}
+
+	public int getMaxCachedStrata() {
+		return maxCachedStrata;
+	}
+
+	public void setMaxCachedStrata(int maxCachedStrata) {
+		this.maxCachedStrata = maxCachedStrata;
+		//TODO
+	}
+
+	public Snapshot<StateT> getCurrentState() {
+		return currentState;
+	}
+
+	public void save() {
+		//TODO
+	}
 
 }
