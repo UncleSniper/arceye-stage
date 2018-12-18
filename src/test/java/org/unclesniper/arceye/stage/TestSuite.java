3d27f7c0aafd2c87c76aa7e321bcd74a82eb0384
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2018-12-18 15:27:48 +0000

    continued hikefile

---
+++ src/test/java/org/unclesniper/arceye/stage/TestSuite.java
@@ -0,0 +1,10 @@
+package org.unclesniper.arceye.stage;
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	StageFileTests.class,
+})
+public class TestSuite {}
